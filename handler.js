8ca856f33ca40e083b5abfd5b6d5e6ad93b4d30a
Philipp Muens <raute1337@gmx.de>
2016-10-10 16:16:26 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -3,7 +3,12 @@
 const create = require('./create.js');
 
 module.exports.create = (event, context, callback) => {
-  create(event, (error, response) => {
-    context.done(error, response);
+  create(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
   });
 };
